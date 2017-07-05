ee414b990ebbcdffa87ff82ee22fc5b13b64d8f5
Ahsan Sohail <k142109@nu.edu.pk>
2017-07-05 22:30:49 +0500

    Header Component

--- autopart/src/app/app.module.ts
+++ autopart/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Header } from './components/header/header';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, Header
   ],
   imports: [
     BrowserModule

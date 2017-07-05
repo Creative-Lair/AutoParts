df0a47e6dd97576baa551263f2c86ff5e263c47d
Ahsan Sohail <k142109@nu.edu.pk>
2017-07-05 22:48:11 +0500

    Body

--- autopart/src/app/app.module.ts
+++ autopart/src/app/app.module.ts
@@ -5,11 +5,12 @@ import { AppComponent } from './app.component';
 import { Header } from './components/header/header';
 import { Slider } from './components/slider/slider';
 import { Footer } from './components/footer/footer';
+import { Body } from './components/body/body';
 
 
 @NgModule({
   declarations: [
-    AppComponent, Header, Slider, Footer
+    AppComponent, Header, Slider, Footer, Body
   ],
   imports: [
     BrowserModule
---
+++ autopart/src/app/components/body/body.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-body',
+  templateUrl: 'body.html',
+})
+export class Body {
+  
+}
